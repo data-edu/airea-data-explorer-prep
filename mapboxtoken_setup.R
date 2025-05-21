52ef3c367e28d1c6ef223027d9d0511d0b04158e
Joshua Rosenberg <jmrosenberg@utk.edu>
2025-05-21 15:31:18 -0400

    add mapbox token blank file

---
+++ mapboxtoken_setup.R
@@ -0,0 +1,7 @@
+mapbox_token <- ""
+
+if (mapbox_token == "YOUR_MAPBOX_ACCESS_TOKEN") {
+  message("PLEASE NOTE - You need to set up your mapbox token, see the README")
+} else {
+  message("mapbox token set")
+}
