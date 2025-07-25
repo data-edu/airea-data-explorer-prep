70d7cad470b68488318736a813f67d34a7d6c70e
Joshua Rosenberg <jmrosen48@gmail.com>
2025-07-25 15:40:57 -0400

    edit prep data

--- prep/prep-data-for-app.R
+++ prep/prep-data-for-app.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(haven)
 
+# supply
 supply_data <- read_dta("prep/ccrc_cip_comp_aire_6dig.dta")
-
-demand_data <- read_csv("prep/lightcast-soc-year-county-2025-02-24 7_23pm.csv")
-
 cz_labels <- read_csv("cz-labels.csv")
+
+# demand
+demand_data <- read_csv("prep/lightcast-soc-year-county-2025-02-24 7_23pm.csv")
+demand_meta_data <- read_dta("prep/aire_soc.dta")
+cz_labels
+
+
