1258fc5c5f995233f8f1379420f5645f0cf42319
Joshua Rosenberg <jmrosen48@gmail.com>
2025-09-03 17:00:02 -0400

    update data based on new supply and CT issue for demand

--- loadtest.R
+++ loadtest.R
@@ -1,4 +1,9 @@
 library(shinyloadtest)
 shinyloadtest::record_session("http://127.0.0.1:5790/")
 
-shinycannon recording.log http://127.0.0.1:5790/ --workers 5 --loaded-duration-minutes 2 --output-dir run1
+shinycannon recording.log http://127.0.0.1:5790/ \
+--workers 20 \
+--loaded-duration-minutes 5 \
+--output-dir loadtest-20
+
+shinyloadtest_report("loadtest-20")
